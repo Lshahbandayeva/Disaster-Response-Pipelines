a807255078e0cc1ecc49004eaf60f8b66732aed6
Lshahbandayeva <shah.lale97@gmail.com>
2020-05-13 06:06:11 +0000

    ML Pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,90 @@
 import sys
+import pandas as pd
+import numpy as np
+import re
+import pickle
+
+from sqlalchemy import create_engine
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.multioutput import MultiOutputClassifier
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+nltk.download('wordnet') # download for lemmatization
+nltk.download('stopwords')
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///InsertDatabaseName.db')
+    df = pd.read_sql_table('InsertTableName', engine)
+    X = df['message']
+    Y = df[df.columns[4:]]
+    category_names = list(df.columns[4:])
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = re.sub(url_regex, 'urlplaceholder', text.lower())
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+    ('feature_pipeline', FeatureUnion([
+
+        ('text_pipeline', Pipeline([
+            ('vect', CountVectorizer(tokenizer = tokenize)),
+            ('tfidf', TfidfTransformer())
+        ]))
+
+    ])),
+
+    ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {
+        'clf__estimator__max_depth': [2, 3]
+    }
+
+    grid_search = GridSearchCV(pipeline, param_grid = parameters, cv = 3, n_jobs = -1)
+    return grid_search
+    
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+    y_pred = model.predict(X_test)
+    y_pred = pd.DataFrame(y_pred)
+    i = 0
+    for col in y_test.columns:
+        print("Accuracy score: ", accuracy_score(y_test[col], y_pred[i]))
+        print(classification_report(y_test[col], y_pred[i]))
+        i += 1
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath), "wb")
 
 
 def main():
